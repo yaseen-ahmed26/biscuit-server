081a8f9e46753c34845228079f4075e8f8148121
yaseen-ahmed26 <yaseen.ahmed26@outlook.com>
2026-08-04 11:42:57 +0100

    feat: ignore casing for .env variables

--- app/config.py
+++ app/config.py
@@ -7,6 +7,7 @@ class Settings(BaseSettings):
     model_config = SettingsConfigDict(
         env_file = ".env",
         env_file_encoding = "utf-8",
+        case_sensitive = False
     )
 
     secret_key: SecretStr
